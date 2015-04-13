19360dd5a73b456b32dfb21eff0d33408d58d52f
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2015-04-12 22:52:43 -0700

    fix host numbering

--- scripts/00_prepare_environment.sh
+++ scripts/00_prepare_environment.sh
@@ -5,10 +5,10 @@ echo "Host *" > ~/.ssh/config
 echo "    StrictHostKeyChecking no" > ~/.ssh/config
 echo "    UserKnownHostsFile=/dev/null" > ~/.ssh/config
 
-ssh $CLUSTERNAME-mon01 apt-get -y install ntp
-ssh $CLUSTERNAME-mon02 apt-get -y install ntp
-ssh $CLUSTERNAME-mon03 apt-get -y install ntp
-ssh $CLUSTERNAME-osd01 apt-get -y install ntp
-ssh $CLUSTERNAME-osd02 apt-get -y install ntp
-ssh $CLUSTERNAME-osd03 apt-get -y install ntp
-ssh $CLUSTERNAME-osd04 apt-get -y install ntp
+ssh $CLUSTERNAME-mon1 apt-get -y install ntp
+ssh $CLUSTERNAME-mon2 apt-get -y install ntp
+ssh $CLUSTERNAME-mon3 apt-get -y install ntp
+ssh $CLUSTERNAME-osd1 apt-get -y install ntp
+ssh $CLUSTERNAME-osd2 apt-get -y install ntp
+ssh $CLUSTERNAME-osd3 apt-get -y install ntp
+ssh $CLUSTERNAME-osd4 apt-get -y install ntp
